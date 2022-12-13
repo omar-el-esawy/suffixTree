58a27f6f359f50272559251680d0217020c58c5e
Omar El-Esawy <75210545+omar-el-esawy@users.noreply.github.com>
2022-12-13 21:21:46 +0200

    some modifications

--- main.cpp
+++ main.cpp
@@ -32,18 +32,18 @@ struct Node {
 };
 
 class SuffixTree {
-    char *s;
+    const char *s;
     Node *root;
     int sz;
 public:
 
-    SuffixTree(char *string) {
+    SuffixTree(const char *string) {
         int n = 26;
         this->s = string;
         sz = strlen(string);
         root = new Node(0, sz, 0);
-        root->add(new Node(sz - 1, 1, sz - 1));
         build();
+        root->add(new Node(sz - 1, 1, sz - 1));
     }
 
     void build() {
@@ -63,7 +63,8 @@ public:
                 len = 0;
                 ls = i;
                 opar = par->children[i];
-                while (s[ind + len] == s[par->children[i]->ost + len] and i + len < sz)len++;
+                while (s[ind + len] == s[par->children[i]->ost + len] and i + len < sz)
+                    len++;
             }
         }
         if (len == -1) {
@@ -78,13 +79,14 @@ public:
             lead = new Node(opar->ost + len, opar->len - len, -1);
             lead->children = opar->children;
             lead->lastIndex = opar->lastIndex;
+            build(npar, ind + len);
         } else
             lead = new Node(opar->ost + len, opar->len - len, opar->tst);
         npar->add(child);
         npar->add(lead);
         par->add(npar, ls);
         delete opar;
-        build(npar, ind + len);
+
     }
 
 
@@ -99,7 +101,7 @@ public:
         }
     }
 
-    Node *Search(Node *par, char *cstring, int ind) {
+    Node *Search(Node *par, const char *cstring, int ind) {
 
         int len = -1;
         if (cstring[ind] == s[par->ost]) {
@@ -113,22 +115,26 @@ public:
         else if (ind + len == strlen(cstring))return par;
         else {
             for (int i = 0; i < par->lastIndex; i++) {
-                return Search(par->children[i], cstring, ind + len);
+                if (s[par->children[i]->ost] == cstring[ind + len - 1])
+                    return Search(par->children[i], cstring, ind + len - 1);
             }
         }
+        return nullptr;
     }
 
     void DFS(Node *node) {
-        if (node->tst != -1)std::cout << node->tst << '\n';
+        if (node->tst != -1)std::cout << node->tst << ' ';
         for (int i = 0; i < node->lastIndex; i++) {
             DFS(node->children[i]);
         }
     }
 
-    void Search(char *cstring) {
+    void Search(const char *cstring) {
         for (int i = 0; i < root->lastIndex; i++) {
             Node *node = Search(root->children[i], cstring, 0);
-            if (node != nullptr)DFS(node);
+            if (node != nullptr) {
+                DFS(node);
+            }
         }
 
     }
@@ -147,8 +153,10 @@ int main() {
 
     SuffixTree t("banana$");
     t.print();
-    t.Search("ana");
 
+    t.Search("ana"); // Prints: 1 3 7
+    std::cout << '\n';
+    t.Search("bana"); // Prints: 4 8
 
     return 0;
 }
